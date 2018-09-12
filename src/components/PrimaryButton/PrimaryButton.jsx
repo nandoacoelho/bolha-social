754a893f922b08481754e34030590c73c0156e91
Fernando de Almeida Coelho <nandoacoelho@gmail.com>
2018-09-11 23:41:06 -0300

    Update primary button with icons

--- src/components/PrimaryButton/PrimaryButton.jsx
+++ src/components/PrimaryButton/PrimaryButton.jsx
@@ -2,14 +2,26 @@ import React from "react";
 import { Fade } from "react-reveal";
 import styles from "./PrimaryButton.module.scss";
 import Link from "gatsby-link";
-const PrimaryButton = ({ isLink, linkTo, title }) => {
+const PrimaryButton = ({ isLink, linkTo, title, isActive, icon }) => {
   return isLink ? (
-    <a className={styles.primaryButton} href={linkTo}>
+    <a
+      className={`${styles.primaryButton} ${isActive ? styles.active : ""}${
+        icon ? styles.buttonIcon : ""
+      }`}
+      href={linkTo}
+      target="_blank">
       {title}
+      {icon && (
+        <div className={styles.icon}>
+          <img src={icon} />
+        </div>
+      )}
     </a>
   ) : (
     <Link to={linkTo}>
-      <button className={styles.primaryButton} type="button">
+      <button
+        className={`${styles.primaryButton} ${isActive ? styles.active : ""}`}
+        type="button">
         {title}
       </button>
     </Link>
