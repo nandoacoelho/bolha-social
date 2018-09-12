622b57069d889f27db12e06a921525d754aa9f27
Fernando de Almeida Coelho <nandoacoelho@gmail.com>
2018-09-11 23:20:04 -0300

    Update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import styles from './Footer.module.scss';
-import config from '../../../config/SiteConfig';
+import React from "react";
+import styles from "./Footer.module.scss";
+import config from "../../../config/SiteConfig";
 
 const Footer = () => (
   <footer className={styles.footer}>
-    {config.copyright} <a href="https://github.com/LeKoArts/gatsby-starter-portfolio-emma">Emma Theme</a> {' by '}{' '}
-    <a href="https://www.lekoarts.de">LekoArts</a>.
+    {config.copyright} <a href="http://bolhasocial.com">Bolha Social</a>{" "}
+    {" - Todos os direitos reservados - "} <a href="https://ufrj.br">UFRJ</a>.
   </footer>
 );
 
