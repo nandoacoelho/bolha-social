eac2abbfedd24fd0dd01b12cbec1df55d0f0b15e
Fernando de Almeida Coelho <nandoacoelho@gmail.com>
2018-09-11 23:42:34 -0300

    Update profile with linkedin links

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import styles from "./Profile.module.scss";
 import PrimaryButton from "../PrimaryButton/PrimaryButton";
+import linkedinIcon from "../../assets/images/icon-linkedin.png";
 
 const Profile = ({ image, text, name, linkedin }) => (
   <div className={styles.profile}>
     <img src={image} />
     <h2 className={styles.name}>{name}</h2>
     <p className={styles.text}>{text}</p>
-    <PrimaryButton />
+    <div className={styles.buttonWrapper}>
+      <PrimaryButton
+        isLink
+        title="linkedin"
+        icon={linkedinIcon}
+        linkTo={linkedin}
+      />
+    </div>
   </div>
 );
 
